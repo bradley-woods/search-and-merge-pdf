7770521837e07c5bc028ad65153066a6acd0ff13
bwoods4 <bradleywoods4@gmail.com>
2022-10-09 12:49:06 +0100

    update

--- project.py
+++ project.py
@@ -1,35 +1,65 @@
-from PyPDF2 import PdfFileMerger
-
-#Create and instance of PdfFileMerger() class
-merger = PdfFileMerger()
-
-#Create a list with file names
-pdf_files = ['pdf_files/sample_page1.pdf', 'pdf_files/sample_page2.pdf']
-
-#Iterate over the list of file names
-for pdf_file in pdf_files:
-    #Append PDF files
-    merger.append(pdf_file)
-
-#Write out the merged PDF
-merger.write("merged_2_pages.pdf")
-merger.close()
-
+import re
+from PyPDF2 import PdfMerger, PdfReader
 
 def main():
-    ...
+
+    filen = input("Filename: ") 
+    #Create and instance of PdfFileMerger() class
+    merger = PdfMerger()
 
 
-def input():
-    ...
+    temp = open(f'search/example record sheet/{filen}', 'rb')
+    reader = PdfReader(temp)
+
+    first_page = reader.getPage(0)
+
+    text = first_page.extractText().replace(" ","")
+
+    headr = re.findall("PN[0-9]{6}", text)[0]
+    print(headr)
+    batch = re.findall("B[0-9]{5}", text)
+
+    pdf_files = []
+    for filename in batch:
+        pdf_files.append(f"merge/material certs/{filename}.pdf")
+
+    print(pdf_files)
+
+    for pdf_file in pdf_files:
+        merger.append(pdf_file)
+
+    merger.write(f"search/merged material certs/{headr} material certs.pdf")
+    merger.close()
 
 
-def merge():
-    ...
+    #Create a list with file names
 
 
-def output():
-    ...
+
+    #pdf_files = ['pdf_files/sample_page1.pdf', 'pdf_files/sample_page2.pdf']
+
+    #Iterate over the list of file names
+    #for pdf_file in pdf_files:
+        #Append PDF files
+    #   merger.append(pdf_file)
+
+    #Write out the merged PDF
+    #merger.write("merged_2_pages.pdf")
+    #merger.close()
+
+
+
+
+
+#def input():
+
+
+
+#def merge():
+
+
+
+#def output():
 
 
 if __name__ == "__main__":
